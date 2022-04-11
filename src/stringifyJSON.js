0fa002746d3b1a9d5236d9dd5a77ed1e1a8d6286
Andres Arango <ararango@comcast.net>
2022-04-11 13:05:27 -0700

    pass tests for stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -2,7 +2,39 @@
 // var stringifyJSON = JSON.stringify;
 
 // but you don't so you're going to write it from scratch:
-
+var _;
 var stringifyJSON = function(obj) {
-  // your code goes here
+  if (typeof obj === 'number' || typeof obj === 'boolean') {
+    return String(obj);
+  } else if (typeof obj === 'string') {
+    return '"' + obj + '"';
+  } else if (obj === null) {
+    return 'null';
+  } else if (obj === undefined) {
+    return obj;
+  }
+
+  if (Array.isArray(obj)) {
+    // join array on delimiter (,)
+    var stringifiedArray = _.map(obj, function(element) {
+      if (element === undefined || typeof element === 'function') {
+        return 'null';
+      }
+      return stringifyJSON(element);
+    });
+    return '[' + stringifiedArray.join(',') + ']';
+  }
+  // object
+  var stringifiedObject = _.reduce(obj, function(memo, value, key) {
+    if (value === undefined || typeof value === 'function') {
+      return memo;
+    }
+    return memo + stringifyJSON(key) + ':' + stringifyJSON(value) + ','
+  }, '{');
+
+  if (stringifiedObject.length > 1) {
+
+    return stringifiedObject.slice(0, stringifiedObject.length - 1) + '}';
+  }
+  return stringifiedObject + '}';
 };
