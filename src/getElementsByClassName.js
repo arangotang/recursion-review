164d1a70faa9ce8d8b70bdcfb48f554ae7517d9d
Andres Arango <ararango@comcast.net>
2022-04-11 13:32:43 -0700

    initial run-through of getElementsByClassName (pending testing)

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -1,10 +1,27 @@
-// If life was easy, we could just do things the easy way:
-// var getElementsByClassName = function (className) {
-//   return document.getElementsByClassName(className);
-// };
+var getChildrenClasses = function(element, className) {
+  var classyChildren = [];
+  element.childNodes.forEach(function(child) {
+    if (child.classList.includes(className)) {
+      classyChildren.push(child);
+    }
+    if (child.hasChildNodes()) {
+      classyChildren.concat(getChildrenClasses(child))
+    }
+  })
+  return classyChildren;
+}
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
-  // your code here
+var getElementsByClassName = function(className) {
+  var classyElements = [];
+
+  if (document.body.classList.includes(className)) {
+    classyElements.push(document.body);
+  }
+
+  return classyElements.concat(getChildrenClasses(document.body, className));
+
+  // todo: fix spec file, refresh and go through test website
+
+
 };
